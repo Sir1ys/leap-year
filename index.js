0f0ac3c6256ef04bc93bc5b51aca3fa049d9b95c
Sir1ys <sashakoroviy@gmail.com>
2024-03-06 20:31:47 +0000

    tests and functionality for the years which are divisible by 100 but are divisible or not by 400 are written

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function checkTheYear(year) {
-  if (year % 4 !== 0) return `The year ${year} isn't a leap one!`;
+  if (year % 400 === 0) return `The year ${year} is a leap one!`;
 
-  return `The year ${year} is a leap one!`;
+  if (year % 400 !== 0 && year % 100 === 0) {
+    return `The year ${year} isn't a leap one!`;
+  }
+
+  if (year % 4 === 0) return `The year ${year} is a leap one!`;
+
+  return `The year ${year} isn't a leap one!`;
 }
 
 module.exports = checkTheYear;
--- index.test.js
+++ index.test.js
@@ -10,4 +10,14 @@ describe("checkTheYear", () => {
     expect(checkTheYear(2000)).toBe("The year 2000 is a leap one!");
     expect(checkTheYear(2004)).toBe("The year 2004 is a leap one!");
   });
+
+  it("return correct statement if the year is divisible by 400", () => {
+    expect(checkTheYear(1600)).toBe("The year 1600 is a leap one!");
+    expect(checkTheYear(1200)).toBe("The year 1200 is a leap one!");
+  });
+
+  it("return correct statement if the year is not divisible by 400 but divisble by 100", () => {
+    expect(checkTheYear(1800)).toBe("The year 1800 isn't a leap one!");
+    expect(checkTheYear(1400)).toBe("The year 1400 isn't a leap one!");
+  });
 });
