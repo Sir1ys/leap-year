f45afd8aeab5cfb19391ed27b23ee9852af57441
Sir1ys <sashakoroviy@gmail.com>
2024-03-06 20:37:47 +0000

    functionality was refactored

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 function checkTheYear(year) {
-  if (year % 400 === 0) return `The year ${year} is a leap one!`;
-
-  if (year % 400 !== 0 && year % 100 === 0) {
-    return `The year ${year} isn't a leap one!`;
+  switch (true) {
+    case year % 400 === 0:
+      return `The year ${year} is a leap one!`;
+    case year % 4 === 0 && year % 100 !== 0:
+      return `The year ${year} is a leap one!`;
+    default:
+      return `The year ${year} isn't a leap one!`;
   }
-
-  if (year % 4 === 0) return `The year ${year} is a leap one!`;
-
-  return `The year ${year} isn't a leap one!`;
 }
 
 module.exports = checkTheYear;
